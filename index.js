eb1cda88fcbef0adc09d24d53d39c2b7a0ce15a8
mariami <burdulimariami4@gmail.com>
2022-07-20 18:20:48 +0400

    first version done

--- index.js
+++ index.js
@@ -2,21 +2,66 @@ const wrapper = document.getElementById('wrapper');
 
 
 function addBoxes(row, boxCounter) {
-    for(let i = 0; i < boxCounter; i++) {
+    const boxes = []
+    for (let i = 0; i < boxCounter; i++) {
         let box = document.createElement('div');
         box.classList.add('box');
-        row.appendChild(box);
+        boxes.push(box);
     }
+    row.append(...boxes);
 }
 
-
-addRow(22, 22);
-function addRow(rowCounter, boxCounter) {
-    for(let i = 0; i < rowCounter; i ++) {
+function drawMatrix(rowCounter, boxCounter) {
+    wrapper.innerHTML = '';
+    const rows = [];
+    for (let i = 0; i < rowCounter; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
         addBoxes(row, boxCounter);
-        wrapper.appendChild(row);
+
+        rows.push(row);
     }
+
+    wrapper.append(...rows)
 }
+
+function activate(element) {
+    element.classList.add('active');
+}
+
+drawMatrix(10, 10);
+
+let firstBox = document.querySelectorAll('#wrapper .row .box:first-child');
+for (let i = 0; i < firstBox.length; i++) {
+    activate(firstBox[i]);
+}
+let secBox = firstBox.parentElement.nextSibling.firstChild;
+activate(secBox)
+let lastBos = secBox.parentElement.parentElement.lastChild.firstChild;
+activate(lastBos)
+
+
+
+
+let thirdBox = document.querySelector('#wrapper .row:nth-child(2) .box:nth-child(2)');
+activate(thirdBox);
+
+let lastBox = thirdBox.parentElement.lastChild;
+activate(lastBox);
+
+let lastRow = lastBox.parentElement.parentElement.lastChild;
+
+
+for (let i = 0; i < lastRow.childNodes.length; i++) {
+    activate(lastRow.childNodes[i]);
+}
+
+
+
+
+
+
+
+
+
 
